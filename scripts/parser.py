4b0f42403bc762c82f016624e126d823d300176b
sntissera <sntissera123edu@gmail.com>
2025-09-12 19:49:52 +0200

    Update parser.py

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,10 +1,11 @@
 import requests, os, gzip
+import pandas as pd
 
 class Parser:
     '''Parse the FASTA files'''
 
     def __init__ (self,url:str,file_name:str,download_path:str):
-        self.__url = url
+        self.url = url
         self.__file_name = file_name
         self.download_path = download_path
 
@@ -20,7 +21,7 @@ class Parser:
         if not os.path.exists(file_path):
             print(f'Downloading the file to {file_path}')
             try:
-                file = requests.get(self.__url)
+                file = requests.get(self.url)
                 file.raise_for_status()
                 with open(file_path, 'wb') as f:
                     f.write(file.content)
@@ -45,15 +46,31 @@ class Parser:
             open_file = open
             mode = 'r'
 
-        #To test the functionality
-        '''try:
+        headers = []
+        sequences = []
+        current_sequence = []
+        
+        try:
             with open_file(file_path,mode) as f:
-                for i in range(10):
-                    line = f.readline()
-                    if not line:
-                        break
-                    print(line.strip())
-        except FileNotFoundError:
-            print('File not found')
-        except Exception as e:
-            print('Error reading file')'''
+                for line in f:
+                    line = line.strip()
+                    if line.startswith ('>'):
+                        if current_sequence:
+                            sequences.append(''.join(current_sequence))
+                        headers.append(line[1:])
+                        current_sequence = []
+                    else:
+                        current_sequence.append(line)
+                if current_sequence:
+                    sequences.append(''.join(current_sequence))
+
+            df = pd.DataFrame({'Header': headers,
+                           'Sequence': sequences})
+
+            return df
+        
+        except Exception as e: 
+            print(f'Error reading file {e}')
+            return pd.DataFrame()
+
+
